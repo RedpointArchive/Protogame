69e00a26ad0a127079d32d1f9fc6ac39e6d4df1d
James Rhodes <jrhodes@redpointsoftware.com.au>
2013-08-02 20:34:37 +1000

    Added more tests for platforming module.

--- Protogame.Tests/PlatformingModuleTests.cs
+++ Protogame.Tests/PlatformingModuleTests.cs
@@ -18,6 +18,8 @@ namespace Protogame.Tests
             };
         }
     
+        #region Ground tests
+    
         [Fact]
         public void IsNotOnGroundWhenNoEntities()
         {
@@ -89,6 +91,184 @@ namespace Protogame.Tests
                 new[] { player, ground },
                 x => true));
         }
+    
+        #endregion Ground tests
+    
+        #region Speed clamp tests
+    
+        [Fact]
+        public void SpeedIsNotClampedWhenSpeedIsZero()
+        {
+            var kernel = new StandardKernel();
+            kernel.Load<ProtogameIoCModule>();
+            kernel.Load<ProtogamePlatformingIoCModule>();
+            var platforming = kernel.Get<IPlatforming>();
+            
+            var boundingBox = this.CreateBoundingBox(0, 0, 0, 0, 0, 0);
+            platforming.ClampSpeed(boundingBox, 10, 10);
+            Assert.Equal(0, boundingBox.XSpeed);
+            Assert.Equal(0, boundingBox.YSpeed);
+        }
+    
+        [Fact]
+        public void SpeedIsNotClampedWhenSpeedIsUnderLimit()
+        {
+            var kernel = new StandardKernel();
+            kernel.Load<ProtogameIoCModule>();
+            kernel.Load<ProtogamePlatformingIoCModule>();
+            var platforming = kernel.Get<IPlatforming>();
+            
+            var boundingBox = this.CreateBoundingBox(0, 0, 0, 0, 5, 5);
+            platforming.ClampSpeed(boundingBox, 10, 10);
+            Assert.Equal(5, boundingBox.XSpeed);
+            Assert.Equal(5, boundingBox.YSpeed);
+        }
+    
+        [Fact]
+        public void SpeedIsNotClampedWhenSpeedIsNegativeAndUnderLimit()
+        {
+            var kernel = new StandardKernel();
+            kernel.Load<ProtogameIoCModule>();
+            kernel.Load<ProtogamePlatformingIoCModule>();
+            var platforming = kernel.Get<IPlatforming>();
+            
+            var boundingBox = this.CreateBoundingBox(0, 0, 0, 0, -5, -5);
+            platforming.ClampSpeed(boundingBox, 10, 10);
+            Assert.Equal(-5, boundingBox.XSpeed);
+            Assert.Equal(-5, boundingBox.YSpeed);
+        }
+    
+        [Fact]
+        public void SpeedIsNotClampedWhenSpeedIsExactlyAtLimit()
+        {
+            var kernel = new StandardKernel();
+            kernel.Load<ProtogameIoCModule>();
+            kernel.Load<ProtogamePlatformingIoCModule>();
+            var platforming = kernel.Get<IPlatforming>();
+            
+            var boundingBox = this.CreateBoundingBox(0, 0, 0, 0, 10, 10);
+            platforming.ClampSpeed(boundingBox, 10, 10);
+            Assert.Equal(10, boundingBox.XSpeed);
+            Assert.Equal(10, boundingBox.YSpeed);
+        }
+    
+        [Fact]
+        public void SpeedIsNotClampedWhenSpeedIsNegativeAndExactlyAtLimit()
+        {
+            var kernel = new StandardKernel();
+            kernel.Load<ProtogameIoCModule>();
+            kernel.Load<ProtogamePlatformingIoCModule>();
+            var platforming = kernel.Get<IPlatforming>();
+            
+            var boundingBox = this.CreateBoundingBox(0, 0, 0, 0, -10, -10);
+            platforming.ClampSpeed(boundingBox, 10, 10);
+            Assert.Equal(-10, boundingBox.XSpeed);
+            Assert.Equal(-10, boundingBox.YSpeed);
+        }
+    
+        [Fact]
+        public void SpeedIsClampedWhenSpeedIsOverLimit()
+        {
+            var kernel = new StandardKernel();
+            kernel.Load<ProtogameIoCModule>();
+            kernel.Load<ProtogamePlatformingIoCModule>();
+            var platforming = kernel.Get<IPlatforming>();
+            
+            var boundingBox = this.CreateBoundingBox(0, 0, 0, 0, 15, 15);
+            platforming.ClampSpeed(boundingBox, 10, 10);
+            Assert.Equal(10, boundingBox.XSpeed);
+            Assert.Equal(10, boundingBox.YSpeed);
+        }
+    
+        [Fact]
+        public void SpeedIsClampedWhenSpeedIsNegativeAndOverLimit()
+        {
+            var kernel = new StandardKernel();
+            kernel.Load<ProtogameIoCModule>();
+            kernel.Load<ProtogamePlatformingIoCModule>();
+            var platforming = kernel.Get<IPlatforming>();
+            
+            var boundingBox = this.CreateBoundingBox(0, 0, 0, 0, -15, -15);
+            platforming.ClampSpeed(boundingBox, 10, 10);
+            Assert.Equal(-10, boundingBox.XSpeed);
+            Assert.Equal(-10, boundingBox.YSpeed);
+        }
+        
+        #endregion
+    
+        #region Gravity tests
+    
+        [Fact]
+        public void GravityIsAppliedWhenGravityIsPositive()
+        {
+            var kernel = new StandardKernel();
+            kernel.Load<ProtogameIoCModule>();
+            kernel.Load<ProtogamePlatformingIoCModule>();
+            var platforming = kernel.Get<IPlatforming>();
+            
+            var boundingBox = this.CreateBoundingBox(0, 0, 0, 0, 0, 0);
+            platforming.ApplyGravity(boundingBox, 0, 10);
+            Assert.Equal(10, boundingBox.YSpeed);
+            platforming.ApplyGravity(boundingBox, 0, 10);
+            Assert.Equal(20, boundingBox.YSpeed);
+        }
+    
+        [Fact]
+        public void GravityIsAppliedWhenGravityIsNegative()
+        {
+            var kernel = new StandardKernel();
+            kernel.Load<ProtogameIoCModule>();
+            kernel.Load<ProtogamePlatformingIoCModule>();
+            var platforming = kernel.Get<IPlatforming>();
+            
+            var boundingBox = this.CreateBoundingBox(0, 0, 0, 0, 0, 0);
+            platforming.ApplyGravity(boundingBox, 0, -10);
+            Assert.Equal(-10, boundingBox.YSpeed);
+            platforming.ApplyGravity(boundingBox, 0, -10);
+            Assert.Equal(-20, boundingBox.YSpeed);
+        }
+        
+        #endregion
+        
+        #region Apply until tests
+        
+        [Fact]
+        public void ApplyUntilRunsUntilLimit()
+        {
+            var kernel = new StandardKernel();
+            kernel.Load<ProtogameIoCModule>();
+            kernel.Load<ProtogamePlatformingIoCModule>();
+            var platforming = kernel.Get<IPlatforming>();
+            
+            var boundingBox = this.CreateBoundingBox(0, 0, 0, 0, 0, 0);
+            var i = 0;
+            platforming.ApplyActionUntil(
+                boundingBox,
+                x => i++,
+                x => false,
+                10);
+            Assert.Equal(10, i);
+        }
+        
+        [Fact]
+        public void ApplyUntilRunsUntilCheck()
+        {
+            var kernel = new StandardKernel();
+            kernel.Load<ProtogameIoCModule>();
+            kernel.Load<ProtogamePlatformingIoCModule>();
+            var platforming = kernel.Get<IPlatforming>();
+            
+            var boundingBox = this.CreateBoundingBox(0, 0, 0, 0, 0, 0);
+            var i = 0;
+            platforming.ApplyActionUntil(
+                boundingBox,
+                x => i++,
+                x => i >= 10,
+                null);
+            Assert.Equal(10, i);
+        }
+        
+        #endregion
     }
 }
 
