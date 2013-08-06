6b93ea4dc440fc35e905a9e2643c23470f96f359
James Rhodes <jrhodes@redpointsoftware.com.au>
2013-08-06 21:54:04 +1000

    Interception library does not work with Mono debugger attached.

--- Protogame/Performance/ProtogamePerformanceIoCModule.cs
+++ Protogame/Performance/ProtogamePerformanceIoCModule.cs
@@ -10,9 +10,17 @@ namespace Protogame
         public override void Load()
         {
 #if DEBUG
-            this.Bind<IProfiler>().To<DefaultProfiler>().InSingletonScope();
-            var x = new DefaultProfilingInterceptor(this.Kernel.Get<IProfiler>() as DefaultProfiler);
-            this.Kernel.Intercept(p => p.Request.Service.IsInterface).With(x);
+            // Presence of the interception library interferes with the Mono Debugger because
+            // it can't seem to handle the intercepted call stack.  Therefore, under Mono, we
+            // disable the profiler if the debugger is attached.
+            if (!System.Diagnostics.Debugger.IsAttached || System.Type.GetType("Mono.Runtime") == null)
+            {
+                this.Bind<IProfiler>().To<DefaultProfiler>().InSingletonScope();
+                var x = new DefaultProfilingInterceptor(this.Kernel.Get<IProfiler>() as DefaultProfiler);
+                this.Kernel.Intercept(p => p.Request.Service.IsInterface).With(x);
+            }
+            else
+                this.Bind<IProfiler>().To<NullProfiler>().InSingletonScope();
 #else
             this.Bind<IProfiler>().To<NullProfiler>().InSingletonScope();
 #endif
