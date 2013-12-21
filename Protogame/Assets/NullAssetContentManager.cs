b7c7d37e5da9ae02daa7329f7ed4dcdfb52ecb4b
James Rhodes <jrhodes@redpointsoftware.com.au>
2013-12-21 12:50:51 +1100

    Implement NullAssetContentManager

---
+++ Protogame/Assets/NullAssetContentManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Protogame
+{
+    public class NullAssetContentManager : IAssetContentManager
+    {
+        public void SetStream(string assetName, System.IO.Stream stream)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Purge(string assetName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Load<T>(string assetName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UnsetStream(string assetName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
