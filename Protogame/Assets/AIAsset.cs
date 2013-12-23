c590ee1f01209c44b42a55389e2d31d4b50bfd92
James Rhodes <jrhodes@redpointsoftware.com.au>
2013-12-23 11:20:52 +1100

    Added more asset management
    Summary: This adds asset management required for our abilities system including AI assets.
    
    Test Plan: Review code.
    
    Reviewers: jrhodes
    
    CC: Sabre070
    
    Differential Revision: https://code.redpointsoftware.com.au/D142

---
+++ Protogame/Assets/AIAsset.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Protogame
+{
+    public abstract class AIAsset : MarshalByRefObject, IAsset
+    {
+        public string Name { get; private set; }
+
+        public AIAsset(string name, string value)
+        {
+            this.Name = name;
+        }
+
+        public abstract void Update(IGameContext gameContext, IUpdateContext updateContext);
+        public abstract void Render(IGameContext gameContext, IRenderContext renderContext);
+
+        public bool SourceOnly
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        public bool CompiledOnly
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        public T Resolve<T>() where T : class, IAsset
+        {
+            if (typeof(T).IsAssignableFrom(typeof(AIAsset)))
+                return this as T;
+            throw new InvalidOperationException("Asset already resolved to AIAsset.");
+        }
+    }
+}
+
---
+++ Protogame/Assets/AssemblyLoadStrategy.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace Protogame
+{
+    public class AssemblyLoadStrategy : ILoadStrategy
+    {
+        public bool ScanSourcePath
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        public string[] AssetExtensions
+        {
+            get
+            {
+                return new string[0];
+            }
+        }
+
+        public object AttemptLoad(string path, string name)
+        {
+            foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies())
+                foreach (var type in assembly.GetTypes())
+                    if (typeof(IAsset).IsAssignableFrom(type))
+                    {
+                        var attribute = type.GetCustomAttributes(false).Cast<Attribute>().OfType<AssetAttribute>().FirstOrDefault();
+                        if (attribute != null)
+                            if (attribute.Name == name)
+                                return new { Type = type, Name = name };
+                    }
+
+            return null;
+        }
+    }
+}
+
