9b411fa7075788f374b219f96b0a72f266e05c4c
Josh Harvey <jharvey@redpointsoftware.com.au>
2014-02-24 13:10:06 +1100

    Add GamePadThumbstickEvent

---
+++ Protogame/Events/GamePadThumbstickEvent.cs
@@ -0,0 +1,11 @@
+namespace Protogame
+{
+    using Microsoft.Xna.Framework.Input;
+
+    /// <summary>
+    /// Represents an event relating to a thumbstick on a game pad input device.
+    /// </summary>
+    public abstract class GamePadThumbstickEvent : GamePadEvent
+    {
+    }
+}
