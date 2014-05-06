d56cb2544b5170cb61ea8171526ccb07a78ea844
James Rhodes <jrhodes@redpointsoftware.com.au>
2014-05-06 17:28:20 +1000

    Don't create LZMA encoder / decoder for each operation

--- Protogame/Compression/LzmaHelper.cs
+++ Protogame/Compression/LzmaHelper.cs
@@ -35,34 +35,59 @@ namespace Protogame.Compression
 			eos
 		};
 
+        private static LzmaEncoder m_Encoder;
+
+        private static LzmaDecoder m_Decoder;
+
+        private static byte[] m_DecoderProps;
+
+        private static object m_EncoderLock = new object();
+
+        private static object m_DecoderLock = new object();
+
         public static void Compress(Stream inStream, Stream outStream)
         {
-            LzmaEncoder encoder = new LzmaEncoder();
-            encoder.SetCoderProperties(propIDs, properties);
-            encoder.WriteCoderProperties(outStream);
-            long fileSize = inStream.Length;
-            for (int i = 0; i < 8; i++)
-                outStream.WriteByte((Byte)(fileSize >> (8 * i)));
-            encoder.Code(inStream, outStream, -1, -1, null);
+            lock (m_EncoderLock)
+            {
+                if (m_Encoder == null)
+                {
+                    m_Encoder = new LzmaEncoder();
+                    m_Encoder.SetCoderProperties(propIDs, properties);
+                }
+
+                m_Encoder.WriteCoderProperties(outStream);
+                long fileSize = inStream.Length;
+                for (int i = 0; i < 8; i++) outStream.WriteByte((Byte)(fileSize >> (8 * i)));
+                m_Encoder.Code(inStream, outStream, -1, -1, null);
+            }
         }
 
         public static void Decompress(Stream newInStream, Stream newOutStream)
         {
-            LzmaDecoder decoder = new LzmaDecoder();
-            byte[] properties2 = new byte[5];
-            if (newInStream.Read(properties2, 0, 5) != 5)
-                throw (new Exception("input .lzma is too short"));
-            long outSize = 0;
-            for (int i = 0; i < 8; i++)
+            lock (m_DecoderLock)
             {
-                int v = newInStream.ReadByte();
-                if (v < 0)
-                    throw (new Exception("Can't Read 1"));
-                outSize |= ((long)(byte)v) << (8 * i);
+                if (m_Decoder == null)
+                {
+                    m_Decoder = new LzmaDecoder();
+                }
+
+                if (m_DecoderProps == null)
+                {
+                    m_DecoderProps = new byte[5];
+                }
+                if (newInStream.Read(m_DecoderProps, 0, 5) != 5) throw (new Exception("input .lzma is too short"));
+                long outSize = 0;
+                for (int i = 0; i < 8; i++)
+                {
+                    int v = newInStream.ReadByte();
+                    if (v < 0) throw (new Exception("Can't Read 1"));
+                    outSize |= ((long)(byte)v) << (8 * i);
+                }
+
+                m_Decoder.SetDecoderProperties(m_DecoderProps);
+                long compressedSize = newInStream.Length - newInStream.Position;
+                m_Decoder.Code(newInStream, newOutStream, compressedSize, outSize, null);
             }
-            decoder.SetDecoderProperties(properties2);
-            long compressedSize = newInStream.Length - newInStream.Position;
-            decoder.Code(newInStream, newOutStream, compressedSize, outSize, null);
         }
     }
 }
