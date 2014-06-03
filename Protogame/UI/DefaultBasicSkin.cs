caf049b8bddc7aae65ed2b11ac91efc3ee3ff6e9
James Rhodes <jrhodes@redpointsoftware.com.au>
2014-06-04 08:53:30 +1000

    Add DefaultBasicSkin for UI

---
+++ Protogame/UI/DefaultBasicSkin.cs
@@ -0,0 +1,14 @@
+﻿namespace Protogame
+{
+    using Microsoft.Xna.Framework;
+
+    public class DefaultBasicSkin : IBasicSkin
+    {
+        public Color LightEdgeColor { get { return new Color(160, 160, 160); } }
+        public Color BackSurfaceColor { get { return new Color(96, 96, 96); } }
+        public Color SurfaceColor { get { return new Color(128, 128, 128); } }
+        public Color DarkSurfaceColor { get { return new Color(96, 96, 96); } }
+        public Color DarkEdgeColor { get { return new Color(32, 32, 32); } }
+        public Color TextColor { get { return Color.Black; } }
+    }
+}
