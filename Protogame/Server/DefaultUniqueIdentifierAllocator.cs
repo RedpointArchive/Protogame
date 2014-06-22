a323494655b5be7ca006fb1faf3bf70e534ba2ef
James Rhodes <jrhodes@redpointsoftware.com.au>
2014-06-22 17:24:56 +1000

    Expose current value on unique identifier allocator

--- Protogame/Server/DefaultUniqueIdentifierAllocator.cs
+++ Protogame/Server/DefaultUniqueIdentifierAllocator.cs
@@ -9,6 +9,18 @@ namespace Protogame
             this.m_NextUniqueIdentifier = 1;
         }
 
+        public int Current
+        {
+            get
+            {
+                return this.m_NextUniqueIdentifier;
+            }
+            set
+            {
+                this.m_NextUniqueIdentifier = value;
+            }
+        }
+
         public int Allocate()
         {
             return this.m_NextUniqueIdentifier++;
--- Protogame/Server/IUniqueIdentifierAllocator.cs
+++ Protogame/Server/IUniqueIdentifierAllocator.cs
@@ -2,6 +2,8 @@ namespace Protogame
 {
     public interface IUniqueIdentifierAllocator
     {
+        int Current { get; set; }
+
         int Allocate();
     }
 }
