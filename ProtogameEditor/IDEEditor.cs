4382b3e094bf4a02bfb86105cf21a9701a55735e
James Rhodes <jrhodes@redpointsoftware.com.au>
2015-03-30 12:36:28 +1100

    Initial work on IDE editor plugin

---
+++ ProtogameEditor/IDEEditor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ProtogameEditor
+{
+    public class IDEEditor
+    {
+        public void Init()
+        {
+            Console.WriteLine("This is from the editor assembly");
+        }
+
+        public string[] GetHandledFileExtensions()
+        {
+            return new[]
+            {
+                ".test"
+            };
+        }
+    }
+}
+
