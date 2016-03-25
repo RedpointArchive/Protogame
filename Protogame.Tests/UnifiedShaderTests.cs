2bf542634a4b206bec9c94832556d6e462882d6b
June Rhodes <jrhodes@redpointgames.com.au>
2016-03-24 20:46:59 -0700

    Implement unified shader parser (work-in-progress)

---
+++ Protogame.Tests/UnifiedShaderTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Prototest.Library.Version1;
+
+namespace Protogame.Tests
+{
+    public class UnifiedShaderTests
+    {
+        private readonly IAssert _assert;
+
+        public UnifiedShaderTests(IAssert assert)
+        {
+            _assert = assert;
+        }
+
+        public void ParseExampleCode()
+        {
+            var parser = new UnifiedShaderParser();
+            var codeStream =
+                Assembly.GetExecutingAssembly().GetManifestResourceStream("Protogame.Tests.UnifiedShaderExample.usl");
+            _assert.NotNull(codeStream);
+            var codeReader = new StreamReader(codeStream);
+            var effect = parser.Parse(codeReader.ReadToEnd());
+        }
+    }
+}
