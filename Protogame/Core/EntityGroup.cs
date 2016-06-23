079b6be6bec9b2e1894ea8d78f4a830372e841cb
June Rhodes <jrhodes@redpointgames.com.au>
2016-06-23 14:23:27 +1000

    Update EntityGroup to propagate additional method calls to children

--- Protogame/Core/EntityGroup.cs
+++ Protogame/Core/EntityGroup.cs
@@ -1,10 +1,12 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Protogame.ATFLevelEditor;
 using Protoinject;
 
 namespace Protogame
 {
-    public class EntityGroup : IEntity, IContainsEntities
+    public class EntityGroup : IContainsEntities, IEventListener<IGameContext>, IEventListener<INetworkEventContext>, IHasLights, IEntity, IServerEntity, INetworkIdentifiable, ISynchronisedObject
     {
         private readonly INode _node;
 
@@ -27,6 +29,14 @@ namespace Protogame
             get { return this.GetAttachedFinalTransformImplementation(_node); }
         }
 
+        public void Update(IServerContext serverContext, IUpdateContext updateContext)
+        {
+            foreach (var child in _node.Children.Select(x => x.UntypedValue).OfType<IServerEntity>())
+            {
+                child.Update(serverContext, updateContext);
+            }
+        }
+
         public void Render(IGameContext gameContext, IRenderContext renderContext)
         {
             foreach (var child in _node.Children.Select(x => x.UntypedValue).OfType<IEntity>())
@@ -41,6 +51,62 @@ namespace Protogame
             {
                 child.Update(gameContext, updateContext);
             }
+        }
+
+        public bool Handle(IGameContext context, IEventEngine<IGameContext> eventEngine, Event @event)
+        {
+            foreach (var child in _node.Children.Select(x => x.UntypedValue).OfType<IEventListener<IGameContext>>())
+            {
+                if (child.Handle(context, eventEngine, @event))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public bool Handle(INetworkEventContext context, IEventEngine<INetworkEventContext> eventEngine, Event @event)
+        {
+            foreach (var child in _node.Children.Select(x => x.UntypedValue).OfType<IEventListener<INetworkEventContext>>())
+            {
+                if (child.Handle(context, eventEngine, @event))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public IEnumerable<ILight> GetLights()
+        {
+            foreach (var child in _node.Children.Select(x => x.UntypedValue).OfType<IHasLights>())
+            {
+                foreach (var light in child.GetLights())
+                {
+                    yield return light;
+                }
+            }
+        }
+
+        public void ReceiveNetworkIDFromServer(IGameContext gameContext, IUpdateContext updateContext, int identifier)
+        {
+            throw new InvalidOperationException(
+                "Entity groups can not receive network IDs.  This indicates an error in the code.");
+        }
+
+        public void ReceivePredictedNetworkIDFromClient(IServerContext serverContext, IUpdateContext updateContext, MxClient client,
+            int predictedIdentifier)
+        {
+            throw new InvalidOperationException(
+                "Entity groups can not receive predicted network IDs.  This indicates an error in the code.");
+        }
+
+        public void DeclareSynchronisedProperties(ISynchronisationApi synchronisationApi)
+        {
+            throw new InvalidOperationException(
+                "Entity groups can not declare synchronised properties.  Do not attach a network synchronisation component to an entity group.");
         }
     }
 }
