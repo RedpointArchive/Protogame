4f500c6833ebd12adbbae9081479e586aabd8a9c
June Rhodes <jrhodes@redpointgames.com.au>
2017-01-22 00:31:31 +1100

    Add API for setting material coefficient mixing type and callbacks

--- Protogame/Physics/DefaultPhysicsWorldControl.cs
+++ Protogame/Physics/DefaultPhysicsWorldControl.cs
@@ -15,14 +15,65 @@ namespace Protogame
         private readonly IPhysicsEngine _physicsEngine;
 
         private Vector3? _pendingGravity;
+        private ContactSettings.MaterialCoefficientMixingType? _pendingCoefficientMixingType;
         private Func<RigidBody, RigidBody, bool> _pendingConsiderAngularVelocityCallback;
+        private Func<RigidBody, RigidBody, float> _pendingCalculateStaticFrictionCallback;
+        private Func<RigidBody, RigidBody, float> _pendingCalculateDynamicFrictionCallback;
+        private Func<RigidBody, RigidBody, float> _pendingCalculateRestitutionCallback;
 
         public DefaultPhysicsWorldControl(IPhysicsEngine physicsEngine)
         {
             _physicsEngine = physicsEngine;
 
             _pendingGravity = null;
+            _pendingCoefficientMixingType = null;
             _pendingConsiderAngularVelocityCallback = null;
+            _pendingCalculateStaticFrictionCallback = null;
+            _pendingCalculateDynamicFrictionCallback = null;
+            _pendingCalculateRestitutionCallback = null;
+        }
+
+        public void SyncPendingChanges()
+        {
+            var physicsWorld = _physicsEngine.GetInternalPhysicsWorld();
+            if (physicsWorld != null)
+            {
+                if (_pendingGravity != null)
+                {
+                    physicsWorld.Gravity = _pendingGravity.Value.ToJitterVector();
+                    _pendingGravity = null;
+                }
+
+                if (_pendingCoefficientMixingType != null)
+                {
+                    physicsWorld.ContactSettings.MaterialCoefficientMixing = _pendingCoefficientMixingType.Value;
+                    _pendingCoefficientMixingType = null;
+                }
+
+                if (_pendingConsiderAngularVelocityCallback != null)
+                {
+                    physicsWorld.ContactSettings.ConsiderAngularVelocityCallback = _pendingConsiderAngularVelocityCallback;
+                    _pendingConsiderAngularVelocityCallback = null;
+                }
+
+                if (_pendingCalculateStaticFrictionCallback != null)
+                {
+                    physicsWorld.ContactSettings.CalculateStaticFrictionCallback = _pendingCalculateStaticFrictionCallback;
+                    _pendingCalculateStaticFrictionCallback = null;
+                }
+
+                if (_pendingCalculateDynamicFrictionCallback != null)
+                {
+                    physicsWorld.ContactSettings.CalculateDynamicFrictionCallback = _pendingCalculateDynamicFrictionCallback;
+                    _pendingCalculateDynamicFrictionCallback = null;
+                }
+
+                if (_pendingCalculateRestitutionCallback != null)
+                {
+                    physicsWorld.ContactSettings.CalculateRestitutionCallback = _pendingCalculateRestitutionCallback;
+                    _pendingCalculateRestitutionCallback = null;
+                }
+            }
         }
 
         public Vector3 Gravity
@@ -50,16 +101,28 @@ namespace Protogame
             }
         }
 
-        public void SyncPendingChanges()
+        public ContactSettings.MaterialCoefficientMixingType MaterialCoefficientMixing
         {
-            if (_pendingGravity != null)
+            get
             {
                 var physicsWorld = _physicsEngine.GetInternalPhysicsWorld();
-                if (physicsWorld != null)
+                if (physicsWorld == null)
                 {
-                    physicsWorld.Gravity = _pendingGravity.Value.ToJitterVector();
-                    _pendingGravity = null;
+                    return _pendingCoefficientMixingType ?? ContactSettings.MaterialCoefficientMixingType.TakeMinimum;
                 }
+
+                return physicsWorld.ContactSettings.MaterialCoefficientMixing;
+            }
+            set
+            {
+                var physicsWorld = _physicsEngine.GetInternalPhysicsWorld();
+                if (physicsWorld == null)
+                {
+                    _pendingCoefficientMixingType = value;
+                    return;
+                }
+
+                physicsWorld.ContactSettings.MaterialCoefficientMixing = value;
             }
         }
 
@@ -85,6 +148,81 @@ namespace Protogame
                 }
 
                 physicsWorld.ContactSettings.ConsiderAngularVelocityCallback = value;
+            }
+        }
+
+        public Func<RigidBody, RigidBody, float> CalculateStaticFrictionCallback
+        {
+            get
+            {
+                var physicsWorld = _physicsEngine.GetInternalPhysicsWorld();
+                if (physicsWorld == null)
+                {
+                    return _pendingCalculateStaticFrictionCallback;
+                }
+
+                return physicsWorld.ContactSettings.CalculateStaticFrictionCallback;
+            }
+            set
+            {
+                var physicsWorld = _physicsEngine.GetInternalPhysicsWorld();
+                if (physicsWorld == null)
+                {
+                    _pendingCalculateStaticFrictionCallback = value;
+                    return;
+                }
+
+                physicsWorld.ContactSettings.CalculateStaticFrictionCallback = value;
+            }
+        }
+
+        public Func<RigidBody, RigidBody, float> CalculateDynamicFrictionCallback
+        {
+            get
+            {
+                var physicsWorld = _physicsEngine.GetInternalPhysicsWorld();
+                if (physicsWorld == null)
+                {
+                    return _pendingCalculateDynamicFrictionCallback;
+                }
+
+                return physicsWorld.ContactSettings.CalculateDynamicFrictionCallback;
+            }
+            set
+            {
+                var physicsWorld = _physicsEngine.GetInternalPhysicsWorld();
+                if (physicsWorld == null)
+                {
+                    _pendingCalculateDynamicFrictionCallback = value;
+                    return;
+                }
+
+                physicsWorld.ContactSettings.CalculateDynamicFrictionCallback = value;
+            }
+        }
+
+        public Func<RigidBody, RigidBody, float> CalculateRestitutionCallback
+        {
+            get
+            {
+                var physicsWorld = _physicsEngine.GetInternalPhysicsWorld();
+                if (physicsWorld == null)
+                {
+                    return _pendingCalculateRestitutionCallback;
+                }
+
+                return physicsWorld.ContactSettings.CalculateRestitutionCallback;
+            }
+            set
+            {
+                var physicsWorld = _physicsEngine.GetInternalPhysicsWorld();
+                if (physicsWorld == null)
+                {
+                    _pendingCalculateRestitutionCallback = value;
+                    return;
+                }
+
+                physicsWorld.ContactSettings.CalculateRestitutionCallback = value;
             }
         }
     }
--- Protogame/Physics/IPhysicsWorldControl.cs
+++ Protogame/Physics/IPhysicsWorldControl.cs
@@ -11,17 +11,24 @@ namespace Protogame
     public interface IPhysicsWorldControl
     {
         /// <summary>
-        /// The gravity in the current world.
-        /// </summary>
-        Vector3 Gravity { get; set; }
-
-        /// <summary>
         /// This is an internal method used to synchronise pending changes to the physics world.
         /// The internal physics world state is not always available when users of this interface
         /// set properties, so this method is called by the engine hook to ensure those changes
         /// are propagated to the physics world when it becomes available.
         /// </summary>
         void SyncPendingChanges();
+
+        /// <summary>
+        /// The gravity in the current world.
+        /// </summary>
+        Vector3 Gravity { get; set; }
+
+        /// <summary>
+        /// The coefficient mixing to use for physics collisions in the world.  When two bodies
+        /// collide, this determines the strategy to use for calculating static friction, dynamic
+        /// friction and restitution between the two bodies.
+        /// </summary>
+        ContactSettings.MaterialCoefficientMixingType MaterialCoefficientMixing { get; set; }
 
         /// <summary>
         /// Optionally set a callback which determines whether or not angular velocity should
@@ -32,5 +39,26 @@ namespace Protogame
         /// </para>
         /// </summary>
         Func<RigidBody, RigidBody, bool> ConsiderAngularVelocityCallback { get; set; }
+
+        /// <summary>
+        /// Optionally set a callback that returns the static friction between two bodies
+        /// when they collide.  This is used if <see cref="MaterialCoefficientMixing"/> is
+        /// set to <see cref="ContactSettings.MaterialCoefficientMixingType.UseCallback"/>.
+        /// </summary>
+        Func<RigidBody, RigidBody, float> CalculateStaticFrictionCallback { get; set; }
+
+        /// <summary>
+        /// Optionally set a callback that returns the dynamic friction between two bodies
+        /// when they collide.  This is used if <see cref="MaterialCoefficientMixing"/> is
+        /// set to <see cref="ContactSettings.MaterialCoefficientMixingType.UseCallback"/>.
+        /// </summary>
+        Func<RigidBody, RigidBody, float> CalculateDynamicFrictionCallback { get; set; }
+
+        /// <summary>
+        /// Optionally set a callback that returns the restitution between two bodies
+        /// when they collide.  This is used if <see cref="MaterialCoefficientMixing"/> is
+        /// set to <see cref="ContactSettings.MaterialCoefficientMixingType.UseCallback"/>.
+        /// </summary>
+        Func<RigidBody, RigidBody, float> CalculateRestitutionCallback { get; set; }
     }
 }
