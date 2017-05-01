962144471dd81dd60aa7d75ff65fbc3528521b7b
June Rhodes <jrhodes@redpointgames.com.au>
2017-05-01 21:58:49 +1000

    Support blended images in forward rendering

--- Protogame/Graphics/Default3DForwardRenderPass.cs
+++ Protogame/Graphics/Default3DForwardRenderPass.cs
@@ -26,8 +26,30 @@ namespace Protogame
 
         public Viewport? Viewport { get; set; }
 
+        /// <summary>
+        /// Clear the depth buffer before this render pass starts rendering.  This allows you to alpha blend
+        /// a 3D forward render pass on top of a 2D render pass, without the 2D render pass interfering
+        /// with the rendering of 3D objects.
+        /// </summary>
+        public bool ClearDepthBuffer { get; set; }
+        
+        public BlendState BlendState { get; set; }
+
         public void BeginRenderPass(IGameContext gameContext, IRenderContext renderContext, IRenderPass previousPass, RenderTarget2D postProcessingSource)
         {
+            if (ClearDepthBuffer)
+            {
+                renderContext.GraphicsDevice.Clear(
+                    ClearOptions.DepthBuffer,
+                    Microsoft.Xna.Framework.Color.Transparent,
+                    renderContext.GraphicsDevice.Viewport.MaxDepth,
+                    0);
+            }
+
+            if (BlendState != null)
+            {
+                renderContext.GraphicsDevice.BlendState = BlendState;
+            }
         }
 
         public void EndRenderPass(IGameContext gameContext, IRenderContext renderContext, IRenderPass nextPass)
--- Protogame/Graphics/I3DForwardRenderPass.cs
+++ Protogame/Graphics/I3DForwardRenderPass.cs
@@ -1,4 +1,6 @@
-﻿namespace Protogame
+﻿using Microsoft.Xna.Framework.Graphics;
+
+namespace Protogame
 {
     /// <summary>
     /// A 3D render pass that uses forward rendering.
@@ -6,5 +8,13 @@
     /// <module>Graphics</module>
     public interface I3DForwardRenderPass : I3DBatchedRenderPass, IRenderPassWithViewport
     {
+        /// <summary>
+        /// Clear the depth buffer before this render pass starts rendering.  This allows you to alpha blend
+        /// a 3D forward render pass on top of a 2D render pass, without the 2D render pass interfering
+        /// with the rendering of 3D objects.
+        /// </summary>
+        bool ClearDepthBuffer { get; set; }
+
+        BlendState BlendState { get; set; }
     }
 }
