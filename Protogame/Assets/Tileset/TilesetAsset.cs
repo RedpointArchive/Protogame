d1ed84face717a66795c661dd7cf143439f78cf5
June Rhodes <jrhodes@redpointgames.com.au>
2017-05-10 12:52:04 +1000

    Implement tileset assets using new asset management APIs

--- Protogame/Assets/Tileset/TilesetAsset.cs
+++ Protogame/Assets/Tileset/TilesetAsset.cs
@@ -1,144 +1,33 @@
 namespace Protogame
 {
-    using System;
-
-    /// <summary>
-    /// The tileset asset.
-    /// </summary>
     public class TilesetAsset : IAsset
     {
-        /// <summary>
-        /// The m_ asset manager.
-        /// </summary>
-        private readonly IAssetManager m_AssetManager;
-
-        /// <summary>
-        /// The m_ texture.
-        /// </summary>
-        private IAssetReference<TextureAsset> m_Texture;
-
-        /// <summary>
-        /// The m_ texture name.
-        /// </summary>
-        private string m_TextureName;
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="TilesetAsset"/> class.
-        /// </summary>
-        /// <param name="assetManager">
-        /// The asset manager.
-        /// </param>
-        /// <param name="name">
-        /// The name.
-        /// </param>
-        /// <param name="textureName">
-        /// The texture name.
-        /// </param>
-        /// <param name="cellWidth">
-        /// The cell width.
-        /// </param>
-        /// <param name="cellHeight">
-        /// The cell height.
-        /// </param>
-        public TilesetAsset(IAssetManager assetManager, string name, string textureName, int cellWidth, int cellHeight)
+        private IAssetReference<TextureAsset> _texture;
+        
+        private string _textureName;
+        
+        public TilesetAsset(
+            string name,
+            string textureName,
+            IAssetReference<TextureAsset> texture, 
+            int cellWidth, 
+            int cellHeight)
         {
-            this.Name = name;
-            this.m_AssetManager = assetManager;
-            this.m_TextureName = textureName;
-            this.CellWidth = cellWidth;
-            this.CellHeight = cellHeight;
+            Name = name;
+            _textureName = textureName;
+            _texture = texture;
+            CellWidth = cellWidth;
+            CellHeight = cellHeight;
         }
+        
+        public int CellHeight { get; }
+        
+        public int CellWidth { get; }
+        
+        public string Name { get; }
 
-        /// <summary>
-        /// Gets or sets the cell height.
-        /// </summary>
-        /// <value>
-        /// The cell height.
-        /// </value>
-        public int CellHeight { get; set; }
+        public IAssetReference<TextureAsset> Texture => _texture;
 
-        /// <summary>
-        /// Gets or sets the cell width.
-        /// </summary>
-        /// <value>
-        /// The cell width.
-        /// </value>
-        public int CellWidth { get; set; }
-
-        /// <summary>
-        /// Gets a value indicating whether compiled only.
-        /// </summary>
-        /// <value>
-        /// The compiled only.
-        /// </value>
-        public bool CompiledOnly
-        {
-            get
-            {
-                return false;
-            }
-        }
-
-        /// <summary>
-        /// Gets the name.
-        /// </summary>
-        /// <value>
-        /// The name.
-        /// </value>
-        public string Name { get; private set; }
-
-        /// <summary>
-        /// Gets a value indicating whether source only.
-        /// </summary>
-        /// <value>
-        /// The source only.
-        /// </value>
-        public bool SourceOnly
-        {
-            get
-            {
-                return false;
-            }
-        }
-
-        /// <summary>
-        /// Gets the texture.
-        /// </summary>
-        /// <value>
-        /// The texture.
-        /// </value>
-        public IAssetReference<TextureAsset> Texture
-        {
-            get
-            {
-                if (this.m_Texture == null)
-                {
-                    this.m_Texture = this.m_AssetManager.Get<TextureAsset>(this.m_TextureName);
-                }
-
-                return this.m_Texture;
-            }
-        }
-
-        /// <summary>
-        /// Gets or sets the texture name.
-        /// </summary>
-        /// <value>
-        /// The texture name.
-        /// </value>
-        public string TextureName
-        {
-            get
-            {
-                return this.m_TextureName;
-            }
-
-            set
-            {
-                this.m_Texture = null;
-                this.m_TextureName = value;
-            }
-        }
-
+        public string TextureName => _textureName;
     }
 }
---
+++ Protogame/Assets/Tileset/TilesetAssetCompiler.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Protogame
+{
+    public class TilesetAssetCompiler : IAssetCompiler
+    {
+        public string[] Extensions => new[] { "tileset" };
+
+        public async Task CompileAsync(IAssetFsFile assetFile, IAssetDependencies assetDependencies, TargetPlatform platform, IWritableSerializedAsset output)
+        {
+            TilesetJson tilesetJson;
+            using (var stream = new StreamReader(await assetFile.GetContentStream().ConfigureAwait(false)))
+            {
+                tilesetJson = JsonConvert.DeserializeObject<TilesetJson>(await stream.ReadToEndAsync().ConfigureAwait(false));
+            }
+
+            output.SetLoader<IAssetLoader<TilesetAsset>>();
+            output.SetString("TextureName", tilesetJson.TextureName);
+            output.SetInt32("CellWidth", tilesetJson.CellWidth);
+            output.SetInt32("CellHeight", tilesetJson.CellHeight);
+        }
+
+        private class TilesetJson
+        {
+            [JsonProperty("textureName")]
+            public string TextureName { get; set; }
+
+            [JsonProperty("cellWidth")]
+            public int CellWidth { get; set; }
+            
+            [JsonProperty("cellHeight")]
+            public int CellHeight { get; set; }
+        }
+    }
+}
