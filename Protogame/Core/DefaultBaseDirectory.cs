c17ceb1aecd887cfa12168c93592ef542eeeee0b
June Rhodes <jrhodes@redpointgames.com.au>
2017-06-17 02:30:26 +1000

    Make sure DefaultBaseDirectory points to the right content location in macOS games

--- Protogame/Core/DefaultBaseDirectory.cs
+++ Protogame/Core/DefaultBaseDirectory.cs
@@ -6,7 +6,24 @@ namespace Protogame
     {
         public DefaultBaseDirectory()
         {
+#if PLATFORM_MACOS
+            var assemblyDirectory = new System.IO.FileInfo(System.Reflection.Assembly.GetExecutingAssembly().Location).DirectoryName;
+            var resourcesDirectory = Path.Combine(assemblyDirectory, "..", "Resources");
+            if (Directory.Exists(Path.Combine(resourcesDirectory, "Content")))
+            {
+                FullPath = new DirectoryInfo(resourcesDirectory).FullName;
+            }
+            else if (Directory.Exists(assemblyDirectory))
+            {
+                FullPath = new DirectoryInfo(assemblyDirectory).FullName;
+            }
+            else
+            {
+                FullPath = Environment.CurrentDirectory;
+            }
+#else
             FullPath = Environment.CurrentDirectory;
+#endif
         }
 
         public string FullPath { get; }
